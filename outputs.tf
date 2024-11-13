affad8c1aa35a1ead321fcfa2ecc198d4d58aaf2
shaikfirose <shaikfirose2002@gmail.com>
2024-11-13 20:10:56 +0530

    Create outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,15 @@
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "public_subnet_id" {
+  value = aws_subnet.public_subnet.id
+}
+
+output "private_subnet_id" {
+  value = aws_subnet.private_subnet.id
+}
+
+output "bucket_name" {
+  value = aws_s3_bucket.example.bucket
+}
